216c961366389e2d557376f776855d703dec1c6c
EmadGKamel <EmadG.Kamel@gmail.com>
2018-03-08 02:30:23 +0200

    using cx_freeze to build

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
-from disutils.core import setup
-import py2exe
+from cx_Freeze import setup, Executable
+
+base = None
 
 
+executables = [Executable("main.py", base=Win32GUI)]
+
+packages = ["idna"]
+options = {
+    'build_exe': {
+
+        'packages':packages,
+    },
+
+}
+
+setup(
+    name = "PassHash",
+    options = options,
+    version = "1.0",
+    description = 'Simple MD5/SHA-1 calculator.',
+    executables = executables
+)
